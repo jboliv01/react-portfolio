10cdc6837ddd2861e033bee64ff6261a61509996
Jonah Oliver <jboliv01@gmail.com>
2023-02-01 17:46:15 -0500

    adding dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {ThemeProvider} from 'next-themes'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
