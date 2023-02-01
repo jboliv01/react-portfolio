84952e6ed2b0f6a041745ebd832194f244607beb
Jonah Oliver <jboliv01@gmail.com>
2023-02-01 11:32:26 -0500

    updating services I offer section and simplyfing my self description

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,8 +52,7 @@ export default function Home() {
           <div className='text-center p-10 py-1'>
             <h2 className='text-5xl py-2 text-teal-600 font-bold md:text-6xl'>Jonah Oliver</h2>
             <h3 className='text-2xl py-2 md:text-3xl'>Data Analyst and Programmer</h3>
-            <p className='text-med py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto'>Skilled data professional with experience in ETL pipeline building, data extraction, and dashboard creation. 
-            I have a strong background in using technologies such as Python, SQL Server, Azure DevOps, PowerBI and more.
+            <p className='text-med py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto'>Skilled data professional offering data-driven solutions to unlock the potential of your data.
             </p>
           </div>
           <div className='text-5xl flex justify-center gap-16 py-3 text-gray-700'>
@@ -70,12 +69,12 @@ export default function Home() {
         <section className='mx-auto justify-center '>
           <div className='mx-auto justify-center pb-20'>
             <h3 className='text-3xl py-1'>Services I Offer</h3>
-            <p className='text-md py-2 leading-8 text-gray-80'>Since the beginning of my journey as a freelance designer
-            and developer, I've done remote work for <span className='text-teal-500'>agencies </span>
-            consulted for <span className='text-teal-500'>startups</span> and collaborated with talented people to create
-            digital products for both business and consumer use.
+            <p className='text-md py-2 leading-8 text-gray-80'>
+            As a highly skilled data professional, I specialize in delivering data-driven solutions that help organizations unlock the full potential of their data. 
+            My expertise includes building ETL pipelines, extracting and transforming data, and creating interactive dashboards. 
             </p>
-            <p className='text-md py-2 leading-8 text-gray-80'> I offer from a wide range of services, including brand design, programming and teaching.
+            <p className='text-md py-2 leading-8 text-gray-80'>Utilizing technologies such as <span className='text-teal-500'>Python, SQL Server, Azure DevOps, PowerBI and more</span>, 
+            I am well-equipped to provide end-to-end data services to organizations of all sizes.
             </p>
           </div>
           <div className='flex flex-wrap lg:gap-10 mx-auto justify-center'>
